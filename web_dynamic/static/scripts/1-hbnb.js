6cee13416b7867d531419f9db766d03bd6f6611e
Jenny <5704@holbertonstudents.com>
2023-04-28 13:10:37 +1000

    Updated to take in amenity name

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
         var amenityId = this.getAttribute('data-id');
 
         if (this.checked) {
-            var $h4 = $("h4.amenities");
             amenityIdList.push(amenityId);
         } else {
             amenityIdList = amenityIdList.filter(function(amenity) {
@@ -16,9 +15,11 @@ $(document).ready(function() {
         updateH4();
     }
     function updateH4() {
-        var amenityList = amenityIdList.join(', ');
-        $("div.amenities h4").text(amenityList);
-    }
+        var amenityList = amenityIdList.map(function(amenityId) {
+            return $('[data-id="' + amenityId + '"]').data('name');
+          }).join(', ');
+          $("div.amenities h4").text(amenityList);
+        }
     for (let i = 0; i < checkboxes.length; i++) {
         checkboxes[i].addEventListener("change", checkBox);
     }
